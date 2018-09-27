8ac1e177a3bca729cc03366f02c89a15f2b95660
Tony Bowden <tony@tmtm.com>
2018-09-27 06:57:11 +0100

    pick up all who started or finished after 2015
    This won't get all long-serving members, but those should be in the Template

--- scraper.rb
+++ scraper.rb
@@ -8,14 +8,16 @@ names = EveryPolitician::Wikidata.wikipedia_xpath(
   xpath: '//table//a[not(@class="new")]/@title',
 )
 
-# pick up any who are no longer active (and thus not in template)
+# pick up any who have started or finished after 2015
+# This won't get all long-serving members, but those should be in the Template
 sparq = <<EOQ
   SELECT ?item WHERE {
     ?item p:P39 ?posn .
-    ?posn ps:P39 wd:Q18524027 ; pq:P582 ?end .
-    FILTER (?end >= "2015-12-03T00:00:00Z"^^xsd:dateTime) .
+    ?posn ps:P39 wd:Q18524027 ; pq:P580 ?start .
+    OPTIONAL { ?posn pq:P582 ?end }
+    FILTER ((?start >= "2015-12-03T00:00:00Z"^^xsd:dateTime) ||  (?end >= "2015-12-03T00:00:00Z"^^xsd:dateTime))
   }
 EOQ
-exmembers = EveryPolitician::Wikidata.sparql(sparq)
+ids = EveryPolitician::Wikidata.sparql(sparq)
 
-EveryPolitician::Wikidata.scrape_wikidata(ids: exmembers, names: { en: names })
+EveryPolitician::Wikidata.scrape_wikidata(ids: ids, names: { en: names })
